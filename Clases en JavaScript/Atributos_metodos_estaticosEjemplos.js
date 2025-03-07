f812db23bd9ae5b7cbef18fa88ee6477c554f4ee
Axel Sanchez Valdez <axel.sanchez@ulv.edu.mx>
2025-03-07 10:18:31 -0600

    Aun mas cambios

--- Clases en JavaScript/Atributos_metodos_estaticosEjemplos.js
+++ Clases en JavaScript/Atributos_metodos_estaticosEjemplos.js
@@ -1,32 +1,38 @@
 //Creación de clase
 class Alumno{
     static contador = 0;
+    static contador1 = 1
     constructor(nombre, edad, carrera){
         this.nombre = nombre;
         this.edad = edad;
         this.carrera = carrera;
         Alumno.contador++;
+        Alumno.contador1++;
     }
+    //Método
     mostrarInformacion(){
-        return `Nombre: ${this.nombre} | Edad: ${this.edad} | Carrera: ${this.carrera}`;
+        return `Alumno ${Alumno.contador} = Nombre: ${this.nombre} | Edad: ${this.edad} | Carrera: ${this.carrera}`;
     }
-    static obtenerTotalPersonas(){
+    //Método Estático
+    static obtenerTotalAlumnos(){
         return Alumno.contador;
     }
 }
 console.log(`Registro de los alumnos.`);
+//Ingresar la cantidad de alumnos para entrar en una condición
 let message = parseInt(prompt(`¿Cuántos alumnos hay en el cuarto?`));
 if (message == 4){
+    //Ciclo for cuando cumpla esta condición
     for(let i = 0; i < 4; i++){
     //Creación de objetos(alumnos)
-    const personas = new Alumno(prompt(`Ingresa el nombre del alumno.`), prompt(`Ingresa la edad del alumno.`), prompt(`Ingresa la carrera del alumno.`));
-    console.log(personas.mostrarInformacion());
+    const alumnos = new Alumno(prompt(`Ingresa el nombre del alumno ${Alumno.contador1}.`), prompt(`Ingresa la edad del alumno ${Alumno.contador1}.`), prompt(`Ingresa la carrera del alumno ${Alumno.contador1}.`));
+    console.log(alumnos.mostrarInformacion());
     }
-    //Método Estático
-    console.log(`La cantidad de alumnos es: ` + Alumno.obtenerTotalPersonas());
+    //Llamar método Estático
+    console.log(`La cantidad de alumnos es: ` + Alumno.obtenerTotalAlumnos());
 } else if (message <= 3) {
     console.log("El cuarto necesita 4 alumnos.");
-} else{
+} else {
     console.log("La cantidad de alumnos ha excedido el cupo del cuarto.");
 }
 
